d9bb9a3f9b4bf1e494059544afb9f32677711f17
Volodymyr Iatsyshyn <viatsyshyn@gmail.com>
2018-04-14 13:04:39 +0300

    more tuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -782,17 +782,13 @@
 
 // @section machine
 
-// The size of the print bed
-#define X_BED_SIZE 210
-#define Y_BED_SIZE 190
-
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 190
+#define X_MAX_POS 210
+#define Y_MAX_POS 210
+#define Z_MAX_POS 180
 
 /**
  * Software Endstops
@@ -1034,7 +1030,7 @@
 // - Move the Z probe (or nozzle) to a defined XY point before Z Homing when homing all axes (G28).
 // - Prevent Z homing when the Z probe is outside bed area.
 //
-#define Z_SAFE_HOMING
+//#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT ((X_BED_SIZE) / 2)    // X point for Z homing when homing all axes (G28).
